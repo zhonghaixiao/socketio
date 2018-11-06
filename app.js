6342026325f55fef879214d50caf644479d0b13d
zhonghaixiao <1943477488@qq.com>
2018-11-06 15:24:54 +0800

    gps数据采集服务端

--- app.js
+++ app.js
@@ -9,23 +9,23 @@ var port = 2000;
 var observer;
 
 net.createServer(function(sock){
-	console.log('CONNECTED: ' + sock.remoteAddress + 
+	console.log('CONNECTED: ' + sock.remoteAddress +
 			":" + sock.remotePort);
-	
+
 	var temp;
 	let start = false;
 	var gpllBuf = '';
-	
+
 	sock.on('data', function(data){
-		
+
 		var gps_data = data.toString('ascii');
-		
+
 		if(gps_data.indexOf("$GPGLL") > -1){
 			start = true;
 			gpllBuf = gpllBuf.concat(gps_data.slice(gps_data.indexOf("$GPGLL")));
 			console.log("start = " + start + "\n--->" + gpllBuf);
 		}
-		
+
 		if(start === true){
 			if(gps_data.indexOf("A,A*") > -1){
 				gpllBuf = gpllBuf.concat(
@@ -33,41 +33,41 @@ net.createServer(function(sock){
 				);
 				start = false;
 				console.log("gpllBuf: " + gpllBuf + "--end---");
-				
+
 				let temp = gpllBuf.split(',');
 				//let gpsInfo = [temp[1],temp[2],temp[3],temp[4],temp[5]];
 				let gpsInfo = [temp[1],temp[2]];
 				console.log(gpsInfo);
-				
+
 				if(typeof observer === "function"){
 					observer(gpsInfo);
 				}
-				
+
 				/*for(var observer of observers){
 					observer(gpsInfo);
 				}*/
-				
+
 				gpllBuf = "";
-				
+
 			}else{
 				gpllBuf = gpllBuf.concat(gps_data);
 			}
 		}
-		
-		
-		
+
+
+
 	});
-	
+
 	sock.on('close',function(data){
 		console.log('CLOSEDD: ' + sock.remoteAddress + ' '
 				+ sock.remotePort);
 	});
-	
+
 }).listen(port,host);
 
-console.log('tcp server listening on ' + host + ':' + port 
+console.log('tcp server listening on ' + host + ':' + port
 			+ "  reading gps information");
-			
+
 
 const server = require('http').createServer();
 
@@ -80,16 +80,16 @@ io.sockets.on('connection', function(socket){
 
    observer = function(gps){
      console.log("gps: " + gps);
-     socket.emit("gpsData",gps);
+     socket.emit("gpsData",JSON.stringify(gps));
    }
 
   console.log('user connected!');
-  
+
 //  setInterval(() => {
 //	  let realData = [Math.random(), Math.random()];
 //	  socket.emit("gps", JSON.stringify(realData));
 //  }, 500);
- 
+
 
 });
 
